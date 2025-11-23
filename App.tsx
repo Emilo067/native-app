f15649e59be737dd8dfdeb27bde299db4d99713c
Emilo067 <gasanovemil467@gmail.com>
2025-11-23 15:27:42 +0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {Button, Dimensions, Image, StyleSheet, Text, TextInput, View} from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const width = Dimensions.get('window').width
+    return (
+        <View style={styles.container}>
+            <View style={styles.content}>
+                    <Image resizeMode={'contain'} style={styles.logo} source={require('./assets/logo.png')}/>
+                <View style={styles.inputWrapper}>
+                    <TextInput style={styles.input}>Email</TextInput>
+                    <TextInput style={styles.input}>Пароль</TextInput>
+                    <Button title={'Войти'}/>
+                </View>
+                    <Button title={'Восстановить пароль'}/>
+            </View>
+
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        // height: Dimensions.get('screen').height,
+        flex: 1,
+        justifyContent: 'center',
+        padding: 55,
+        backgroundColor: '#16171D'
+    },
+    content: {
+        gap: 50
+    },
+    inputWrapper: {
+        gap: 16
+    },
+    input: {
+      backgroundColor: 'gray'
+    },
+    text: {
+        textAlign: 'center',
+        color: 'blue',
+    },
+    logo: {
+        width: 220,
+        height: 100,
+}
 });
